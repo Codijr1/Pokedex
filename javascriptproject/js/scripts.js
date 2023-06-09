f052f9f255be1e434cfa7e06593da639e34141cc
Codi <cjrobards@gmail.com>
2023-06-09 19:39:43 -0400

    Update
    Adjusted project to be a Pokedex

--- javascriptproject/js/scripts.js
+++ javascriptproject/js/scripts.js
@@ -1,5 +1,19 @@
-alert('Hello World');
+let pokemonList = [
+    {
+    name:'Regirock';
+    height:1.7;
+    type:['rock']
+}
+    {
+    name:'Regice';
+    height:1.8;
+    type:['ice']
+}
+    {
+    name:'Registeel';
+    height:1.9;
+    type:['steel']
+}
+];
 
-let favoriteFood = 'Hibachi is my favorite food';
-
-document.write(favoriteFood);
+//Now, add several objects to the array. Each object represents one Pokémon and needs to have the same keys to avoid errors. Add at least the following three keys to each Pokémon object and make up a value for each of them: name (make this a string, e.g., "Bulbasaur"), height (this will be a number, e.g., 7) and types (make this an array of strings, e.g., ['grass', 'poison']). You can use Pokedex.org as a reference. Create at least three Pokémon objects in your pokemonList array.//
