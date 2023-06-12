df5a1dd179480d8b2e84d3917be4f7cdd21234da
Codi <cjrobards@gmail.com>
2023-06-12 17:31:31 -0400

    Pokedex update
    -names of each Pokemon are now listed
    -the tallest Pokemon is highlighted
    -added custom text and a background color
    -added an image

--- javascriptproject/js/scripts.js
+++ javascriptproject/js/scripts.js
@@ -1,19 +1,32 @@
 let pokemonList = [
     {
-    name:'Regirock';
-    height:1.7;
+    name:'Regirock',
+    height:1.7,
     type:['rock']
-}
+},
     {
-    name:'Regice';
-    height:1.8;
+    name:'Regice',
+    height:1.8,
     type:['ice']
-}
+},
     {
-    name:'Registeel';
-    height:1.9;
+    name:'Registeel',
+    height:1.9,
     type:['steel']
 }
 ];
 
-//Now, add several objects to the array. Each object represents one Pokémon and needs to have the same keys to avoid errors. Add at least the following three keys to each Pokémon object and make up a value for each of them: name (make this a string, e.g., "Bulbasaur"), height (this will be a number, e.g., 7) and types (make this an array of strings, e.g., ['grass', 'poison']). You can use Pokedex.org as a reference. Create at least three Pokémon objects in your pokemonList array.//
+for (let i = 0; i < pokemonList.length; i++){
+    document.write(pokemonList[i].name +"'s height: "+ pokemonList[i].height);
+
+    if (pokemonList[i].height > 1.8){
+        document.write( " <- This one is the tallest")
+    }
+    document.write('<br>')
+}
+
+
+
+
+
+// Next, add code to highlight special Pokémon in your list. Be sure to keep adding comments to explain and document what your code does. Within the loop, add a conditional. The conditional should check if the height is above a certain value (you’re free to pick whatever value you want). If it is, add the note “Wow, that’s big!” to the output. Make sure you set up the conditional so that only one Pokémon has the label “Wow, that’s big!” It could, for example, look like this: “Bulbasaur (height: 7) - Wow, that’s big!”. For example, if you had the following array:
