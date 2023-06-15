bcbb8eabb9799ef5234a7cdf462aff0d69dba54e
Codi <cjrobards@gmail.com>
2023-06-15 19:28:11 -0400

    Update
    -finished formatting the code to use IIFE
    -added a fourth "test" object which is also displayed in the updated array

--- javascriptproject/js/scripts.js
+++ javascriptproject/js/scripts.js
@@ -1,39 +1,61 @@
 let pokemonRepository = (function() {
-    let pokemonList = [
-        {
-        name:'Regirock',
-        height:1.7,
-        type:['rock']
+  let pokemonList = [
+    {
+      name: 'Regirock',
+      height: 1.7,
+      type: ['rock']
     },
-        {
-        name:'Regice',
-        height:1.8,
-        type:['ice']
+    {
+      name: 'Regice',
+      height: 1.8,
+      type: ['ice']
     },
-        {
-        name:'Registeel',
-        height:1.9,
-        type:['steel']
+    {
+      name: 'Registeel',
+      height: 1.9,
+      type: ['steel']
     }
-    ];
+  ];
+//defining the getAll and add functions
+    function getAll() {
+        return pokemonList;
+    }
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+//allows data to be accessed outside of this 
+    return{
+        add: add,
+        getAll: getAll,
+    }
+})();
+
+// Testing the add function
+pokemonRepository.add({
+    name: 'New Pokemon',
+    height: 1.6,
+    type: ['normal']
+  });
+  
+// Function to display the array on the screen
+pokemonRepository.getAll().forEach(function(pokemon) {
+  document.write(pokemon.name + "'s height: " + pokemon.height);
+  if (pokemon.height > 1.8) {
+    document.write(" <- This one is the tallest");
+  }
+  document.write('<br>');
+});
+
+
+/*
             function(getAll){
-                return
+                return pokemonList[i];
             }
 
             function(add){
-            
+                
             }
-})();
 
-pokemonRepository.forEach(function(pokemon){
-    document.write(pokemon.name +"'s height: "+ pokemon.height);
-        if (pokemon.height > 1.8){
-            document.write( " <- This one is the tallest");
-        }
-        document.write('<br>');
-});
-
-/*
 for (let i = 0; i < pokemonList.length; i++){
     document.write(pokemonList[i].name +"'s height: "+ pokemonList[i].height);
 
