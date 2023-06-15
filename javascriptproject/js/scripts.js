dad94b8549762d3025e51f8cd816d2a9aa5f29eb
Codi <cjrobards@gmail.com>
2023-06-15 00:32:20 -0400

    forEach Update
    -code has been formatted to use the forEach function
    -begins to add functions getAll and add
    (unfinished)

--- javascriptproject/js/scripts.js
+++ javascriptproject/js/scripts.js
@@ -1,21 +1,39 @@
-let pokemonList = [
-    {
-    name:'Regirock',
-    height:1.7,
-    type:['rock']
-},
-    {
-    name:'Regice',
-    height:1.8,
-    type:['ice']
-},
-    {
-    name:'Registeel',
-    height:1.9,
-    type:['steel']
-}
-];
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {
+        name:'Regirock',
+        height:1.7,
+        type:['rock']
+    },
+        {
+        name:'Regice',
+        height:1.8,
+        type:['ice']
+    },
+        {
+        name:'Registeel',
+        height:1.9,
+        type:['steel']
+    }
+    ];
+            function(getAll){
+                return
+            }
 
+            function(add){
+            
+            }
+})();
+
+pokemonRepository.forEach(function(pokemon){
+    document.write(pokemon.name +"'s height: "+ pokemon.height);
+        if (pokemon.height > 1.8){
+            document.write( " <- This one is the tallest");
+        }
+        document.write('<br>');
+});
+
+/*
 for (let i = 0; i < pokemonList.length; i++){
     document.write(pokemonList[i].name +"'s height: "+ pokemonList[i].height);
 
@@ -24,9 +42,6 @@ for (let i = 0; i < pokemonList.length; i++){
     }
     document.write('<br>')
 }
+*/
 
-
-
-
-
-// Next, add code to highlight special Pokémon in your list. Be sure to keep adding comments to explain and document what your code does. Within the loop, add a conditional. The conditional should check if the height is above a certain value (you’re free to pick whatever value you want). If it is, add the note “Wow, that’s big!” to the output. Make sure you set up the conditional so that only one Pokémon has the label “Wow, that’s big!” It could, for example, look like this: “Bulbasaur (height: 7) - Wow, that’s big!”. For example, if you had the following array:
+// Next, add code to highlight special Pokémon in your list. Be sure to keep adding comments to explain and document what your code does. Within the loop, add a conditional. The conditional should check if the height is above a certain value (you’re free to pick whatever value you want). If it is, add the note “Wow, that’s big!” to the output. Make sure you set up the conditional so that only one Pokémon has the label “Wow, that’s big!” It could, for example, look like this: “Bulbasaur (height: 7) - Wow, that’s big!”. For example, if you had the following array
