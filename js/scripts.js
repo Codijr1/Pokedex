8249e2249cd4ebd59bc8e1801e92157e4366620d
Codi <cjrobards@gmail.com>
2023-06-20 19:15:06 -0400

    Array Upate
    -array is now imported from an API
    -list of buttons now show all 150 Pokemon from API
    -clicking a button returns the respective Pokemon's details from the API in the terminal via console.log()

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,46 @@
 let pokemonRepository = (function() {
-  let pokemonArray = [
-    {
-      name: 'Regirock',
-      height: 1.7,
-      type: ['rock']
-    },
-    {
-      name: 'Regice',
-      height: 1.8,
-      type: ['ice']
-    },
-    {
-      name: 'Registeel',
-      height: 1.9,
-      type: ['steel']
-    }
-  ];
+  let pokemonArray = [];
+    let api = 'js/pokemonjson.json';
+
+//loadList and loadDetails
+function loadList() {
+  return fetch(api).then(function (response) {
+    return response.json();
+  }).then(function (json) {
+    json.results.forEach(function (item) {
+      let pokemon = {
+        name: item.name,
+        detailsUrl: item.url
+      };
+      add(pokemon);
+    });
+  }).catch(function (e) {
+    console.error(e);
+  })
+}
+
+//allows details to be added
+function loadDetails(item) {
+  let url = item.detailsUrl;
+  return fetch(url).then(function (response) {
+    return response.json();
+  }).then(function (details) {
+    item.imageUrl = details.sprites.front_default;
+    item.height = details.height;
+    item.types = details.types;
+  }).catch(function (e) {
+    console.error(e);
+  });
+}
+
 //defining the getAll and add functions
     function getAll() {
-        return pokemonArray;
-    }
-    function add(pokemon) {
-        pokemonArray.push(pokemon);
-    }
+          return pokemonArray;
+      }
+      function add(pokemon) {
+          pokemonArray.push(pokemon);
+      }
+
 //addList, button, and functions
     function addListItem(pokemon){
       let pokemonlistparent= document.querySelector('.htmlpokemonlist');
@@ -36,25 +54,33 @@ let pokemonRepository = (function() {
         showDetails(pokemon);
       });
     }
+
 //showDetails function
-    function showDetails(pokemon){
-      console.log(pokemon.name);
-    }
+function showDetails(pokemon) {
+  loadDetails(pokemon).then(function () {
+    console.log(pokemon);
+  });
+}
+
 //allows data to be accessed outside of this 
     return{
       add: add,
       getAll: getAll,
-      addListItem: addListItem
+      addListItem: addListItem,
+      loadList: loadList,
+      loadDetails: loadDetails,
+      showDetails: showDetails
   }
 })();
 
-// Testing the add function
-pokemonRepository.add({
-    name: 'New Pokemon',
-    height: 1.6,
-    type: ['normal']
+
+//function allowing the data from the API to be loaded into the array
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
   });
-  
+});
+
 // Function to display the array on the screen
 pokemonRepository.getAll().forEach(function(pokemon) {
   pokemonRepository.addListItem(pokemon);
